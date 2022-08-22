5f6c680c2e474d9eb048f16ae48b4e911fdeb036
Den <denis032014@bk.ru>
2022-08-22 22:09:11 +0300

    add_moda_ltelegram

--- conf/telegram.php
+++ conf/telegram.php
@@ -5,7 +5,7 @@
 
 $feedback__name = $_POST['feedback__name'];
 $feedback__tel = $_POST['feedback__tel'];
-$feedback__words = $_POST['feedback__words'];
+$feed__back_nft_price = $_POST['feed__back_nft_price'];
 $feedback__crypto = $_POST['feedback__crypto'];
 $token = "5546941826:AAF-xuOpCeHepRsfCEnn6ExCuqoos8XUgrM";
 $chat_id = "-1001686101709";
@@ -13,7 +13,6 @@ $arr = array(
   'Name: ' => $feedback__name,
   'Tel: ' => $feedback__tel,
   'Crypto: ' => $feedback__crypto,
-  'About: ' => $feedback__words,
 );
 
 foreach($arr as $key => $value) {
--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,10 @@
+//переменные
+let priceBtc = 100;
+let priceEth = 100;
+let priceEtc = 100
+let feedback__crypto;
+
+
 //для меню
 let coll = document.getElementsByClassName("collapsible");
 
@@ -16,14 +23,13 @@ for (i = 0; i < coll.length; i++) {
 //Плавный скролл
 $('a[href^="#"').on('click', function() {
   let href = $(this).attr('href');
-  if (href != '#') {
+  if ($(href).length) {
     $('html, body').animate({
       scrollTop: $(href).offset().top - 150
   });
   } else return false
 });
 
-let card__nick, card__price
 
 //Добавить нфт в форму
 $('.card__link').on('click', function() {
@@ -33,29 +39,35 @@ $('.card__link').on('click', function() {
   card__nick = $(this).parent().find('.card__nick').text();
   card__price = $(this).parent().find('.card__price').attr('value');
   $('#feed__back_tiker').val(card__nick)
-  $('#feed__back_words').val(card__price)
+  $('#feed__back_nft_price').val(card__price)
 });
 
 
+$('#back').on('click', function() {
+  $('#feedback__input').css("opacity", "1");
+  $('#form__input2').css("display", "none");
+});
 
-//Запрос текущей стоимости крипты
+
+//Запрос текущей стоимости крипты (работает)
 $.get('https://production.api.coindesk.com/v2/tb/price/ticker?assets=BTC,ETH,ETC',
-      function(data) {
-        priceBtc = data.data.BTC.ohlc.c.toFixed(2)
-        priceEth = data.data.ETH.ohlc.c.toFixed(2)
-        priceEtc = data.data.ETC.ohlc.c.toFixed(2);
-      });
+function(data) {
+priceBtc = data.data.BTC.ohlc.c.toFixed(2)
+priceEth = data.data.ETH.ohlc.c.toFixed(2)
+priceEtc = data.data.ETC.ohlc.c.toFixed(2)
+  });
+
+
 
 // Отправка данных
   $(function ($) {
-    $("#feed__back_send").click(function (e) {
+    $("#feed__back_send").on('click', function (e) {
         e.preventDefault()
         feedback__name = $('#feed__back_name').val();
         feedback__tel = $('#feed__back_number').val();
         feedback__tiker = $('#feed__back_tiker').val();
-        feedback__words = $('#feed__back_words').val();
-        feedback__crypto = `The cost of your ${card__nick} in BTC: ${(card__price / priceBtc).toFixed(4)}, ETH: ${(card__price / priceEth).toFixed(4)}, ETC: ${(card__price / priceEtc).toFixed(4)}`;
-
+        feedback__nft_price = $('#feed__back_nft_price').val();
+        
         if (!feedback__name) {
           spop({
             template: 'Please add your name',
@@ -80,25 +92,58 @@ $.get('https://production.api.coindesk.com/v2/tb/price/ticker?assets=BTC,ETH,ETC
             autoclose: 2500
         });  }
 
-        if (feedback__name && feedback__tel && feedback__tiker && feedback__words) {
+
+        
+
+        if (feedback__name && feedback__tel && feedback__tiker && feedback__nft_price) {
+
+          //Запрос котировок, не работает, спросить почему
+          // $.ajax({
+          //   url: 'https://production.api.coindesk.com/v2/tb/price/ticker?assets=BTC,ETH,ETC', 
+          //   method: 'get',
+          //   dataType: 'json',
+          //   success: function(data){ 
+          //     priceBtc = data.data.BTC.ohlc.c.toFixed(2)
+          //     priceEth = data.data.ETH.ohlc.c.toFixed(2)
+          //     priceEtc = data.data.ETC.ohlc.c.toFixed(2)
+          //     console.log(priceBtc)
+          //   },
+          //   error: function (exception) {
+          //     console.log(exception)
+          //     if (exception === 'error' || exception === 'parsererror') {
+          //       priceBtc = 20000
+          //       priceEth = 1500
+          //       priceEtc = 35  
+          //     }
+          // }
+          // });
+          //console.log(priceBtc) вот тут уже не видна переменная, хотя глобально обозначена.
+
+
+          feedback__crypto = `The cost of your ${feedback__tiker} in <strong>BTC:</strong> ${(feedback__nft_price / priceBtc).toFixed(4)}, <i>ETH:</i> ${(feedback__nft_price / priceEth).toFixed(4)}, ETC: ${(feedback__nft_price / priceEtc).toFixed(4)}`;
+
+          //Отправка в телеграм данных
           $.ajax({
             url: "https://nft.rfixit.ru/conf/telegram.php",
             method: "post",
             dataType: "html",
-            data: {feedback__name, feedback__tel, feedback__crypto, feedback__words},
+            data: {feedback__name, feedback__tel, feedback__crypto},
             success: function (data) {
               if (data == "error") { alert("Не удалось отправить") } 
               else {
-                spop({
-                template: '<h4 class="spop-title">Message sent to</h4><a href="https://t.me/+sC81hFOz7jk0MGEy">Telegram</a>',
-                position: 'bottom-right',
-                style: 'success',
-                autoclose: 5000
-                    }); 
+                // location.assign('https://t.me/+sC81hFOz7jk0MGEy')
+                // spop({
+                // template: '<h4 class="spop-title">Message sent to</h4><a href="https://t.me/+sC81hFOz7jk0MGEy">Telegram</a>',
+                // position: 'bottom-right',
+                // style: 'success',
+                // autoclose: 5000
+                //     }); 
           }},
         })
-              $(".feedback__input").trigger('reset')
-        }
+        $("#feedback__input").trigger('reset');
+        $('#feedback__input').css("opacity", "0");
+        $('#form__input2').css("display", "block");
+}
     });
   });
 
@@ -109,7 +154,7 @@ $.get('https://production.api.coindesk.com/v2/tb/price/ticker?assets=BTC,ETH,ETC
     spaceBetween: 50,
     paginationClickable: 1,
     roundLengths: !0,
-    loop:true,
+    loop: true,
     speed: 800,
     grabCursor: !0,
     freeMode: !1,
